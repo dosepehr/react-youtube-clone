67c131919945b658095225628ddcde9971c9a1a3
dosepehr <dosepehr@gmail.com>
2023-02-22 01:15:41 +0330

    make categories data

---
+++ src/constants/data.js
@@ -0,0 +1,28 @@
+import { GrHomeRounded } from 'react-icons/gr';
+import { BsCodeSlash, BsMusicNote, BsCurrencyBitcoin } from 'react-icons/bs';
+import { IoSchool, IoLogoJavascript, IoFitnessOutline } from 'react-icons/io';
+import { SiGooglepodcasts } from 'react-icons/si';
+import { TbBrandNextjs } from 'react-icons/tb';
+import { FaReact, FaTheaterMasks } from 'react-icons/fa';
+import { BiCameraMovie, BiGame } from 'react-icons/bi';
+import { GiGymBag } from 'react-icons/gi';
+import { MdLiveTv, MdCheckroom, MdFaceRetouchingNatural } from 'react-icons/md';
+export const categories = [
+    { name: 'New', icon: <GrHomeRounded /> },
+    { name: 'Javascript', icon: <IoLogoJavascript /> },
+    { name: 'Coding', icon: <BsCodeSlash /> },
+    { name: 'ReactJS', icon: <FaReact /> },
+    { name: 'NextJS', icon: <TbBrandNextjs /> },
+    { name: 'Music', icon: <BsMusicNote /> },
+    { name: 'Education', icon: <IoSchool /> },
+    { name: 'Podcast', icon: <SiGooglepodcasts /> },
+    { name: 'Movie', icon: <BiCameraMovie /> },
+    { name: 'Gaming', icon: <BiGame /> },
+    { name: 'Live', icon: <MdLiveTv /> },
+    { name: 'Sport', icon: <IoFitnessOutline /> },
+    { name: 'Fashion', icon: <MdCheckroom /> },
+    { name: 'Beauty', icon: <MdFaceRetouchingNatural /> },
+    { name: 'Comedy', icon: <FaTheaterMasks /> },
+    { name: 'Gym', icon: <GiGymBag /> },
+    { name: 'Crypto', icon: <BsCurrencyBitcoin /> },
+];
